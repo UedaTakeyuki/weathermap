014068012bbb1b22bdd119acc6963269a955b572
Dr. Takeyuki Ueda <gde00107@nifty.com>
2023-03-19 12:16:00 +0900

    add DISPLAY=:0

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	defer w.Destroy()
 	w.SetTitle("Weather Map")
 	w.SetSize(1200, 800, webview.HintNone)
-//	w.Move(200,200)
+	w.Move(0,0)
 //	w.SetHtml("Thanks for using webview!")
 	w.Navigate("https://openweathermap.org/city/1850916")
 	w.Run()
