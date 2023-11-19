abf82cf75af01fce609f1ae10544fead1cf54a47
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2023-11-19 19:17:39 +0900

    return message of exception

--- main.c
+++ main.c
@@ -11,11 +11,16 @@ static gboolean once_cb(gpointer user_data){
   WebKitWebView *webView = user_data;
 //  webkit_web_view_run_javascript(webView, "window.scrollTo(230,100)", NULL, NULL, NULL);
   webkit_web_view_run_javascript(webView,
-                                 "location.reload();"
-                                 "const footer_panel = document.querySelector('.stick-footer-panel__link');"
-                                 "if (footer_panel != null) {"
-                                 "  footer_panel.click();"
-                                 "}",
+                                 "try {"
+                                 "  location.reload();"
+                                 "  const footer_panel = document.querySelector('.stick-footer-panel__link');"
+                                 "  if (footer_panel != null) {"
+                                 "    footer_panel.click();"
+                                 "  }"
+                                 "} catch (e) {"
+                                 "  return e.name + ": " + e.message"
+                                 "}"
+                                 "return ''",
                                  NULL, NULL, NULL);
   g_print("once_cb done.\n");
 //  return FALSE;
