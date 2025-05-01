a932be6f9dbbd368ad3a974463abf5668ae9a9d0
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2025-05-01 12:55:07 +0900

    add load_changed event callback

--- src/webView/webView.c
+++ src/webView/webView.c
@@ -1,8 +1,44 @@
 #include "webView.h"
 
+// Call back for load-changed event
+static void web_view_load_changed (WebKitWebView  *web_view,
+                                   WebKitLoadEvent load_event,
+                                   gpointer        user_data)
+{
+  const gchar* uri;
+  switch (load_event) {
+  case WEBKIT_LOAD_STARTED:
+    // New load, we have now a provisional URI
+    uri = webkit_web_view_get_uri (web_view);
+    // Here we could start a spinner or update the
+    // location bar with the provisional URI
+    g_print("WEBKIT_LOAD_STARTED: provisional_uri = %s\n", uri);
+    break;
+  case WEBKIT_LOAD_REDIRECTED:
+    uri = webkit_web_view_get_uri (web_view);
+    g_print("WEBKIT_LOAD_REDIRECTED: redirected_uri = %s\n", uri);
+    break;
+  case WEBKIT_LOAD_COMMITTED:
+    // The load is being performed. Current URI is
+    // the final one and it won't change unless a new
+    // load is requested or a navigation within the
+    // same page is performed
+    uri = webkit_web_view_get_uri (web_view);
+    g_print("WEBKIT_LOAD_COMMITTED: uri = %s\n", uri);
+    break;
+  case WEBKIT_LOAD_FINISHED:
+    // Load finished, we can now stop the spinner
+    g_print("WEBKIT_LOAD_FINISHED:\n");
+    break;
+  }
+}
+
 // Create a browser instance
 WebKitWebView * create_browser_instance(){
   WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
+
+  // set call back for load_changed event
+  g_signal_connect(webView, "load_changed", G_CALLBACK(web_view_load_changed), NULL);
 
   // get settings
   WebKitSettings *settings = webkit_web_view_get_settings (webView);
@@ -17,6 +53,6 @@ WebKitWebView * create_browser_instance(){
   // get enable_write_console_messages_to_stdout
   g_warning("enable_write_console_messages_to_stdout: %d",
             webkit_settings_get_enable_write_console_messages_to_stdout(settings));
-
+  
   return webView;
 }
